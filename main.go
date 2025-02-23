435c82b9f601e0ff5ec7dd81a5d0c5bb1c6649ea
unknown <mirzadjohan@gmail.com>
2025-02-23 15:21:49 +0700

    mencoba pattern yang lain

--- main.go
+++ main.go
@@ -80,14 +80,14 @@ func main() {
 		state[i] = make([]int, 5)
 	}
 
-	//state[0][0] = 1
-	//state[0][2] = 1
-	//state[1][1] = 1
-	//state[2][2] = 1
+	state[0][0] = 1
+	state[0][2] = 1
+	state[1][1] = 1
+	state[2][2] = 1
 
-	state[0][1] = 1
-	state[1][0] = 1
-	state[2][1] = 1
+	//state[0][1] = 1
+	//state[1][0] = 1
+	//state[2][1] = 1
 
 	for {
 		clear := exec.Command("clear")
